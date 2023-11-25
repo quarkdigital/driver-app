cfffb6d295a95849bc14af4c221a018f202b0aed
Uros Stesevic <101807204+stesevicuros@users.noreply.github.com>
2023-11-25 15:33:58 +0100

    Connect driver to dispatcher through socket and send his location and… (#1)
    * Connect driver to dispatcher through socket and send his location and status on disconnection
    
    * Muka mi smisljat commit message
    
    * Masa stvari dodah
    
    * Typescript error solved

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import DriverApp from "./src/util/components/Driver";
+import { enableLatestRenderer } from "react-native-maps";
+
+enableLatestRenderer();
 
 export default function App() {
 	return (
@@ -12,7 +15,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
+		display: "flex",
+		width: "100%",
+		height: "100%",
 		backgroundColor: "#fff",
 		alignItems: "center",
 		justifyContent: "center",
